09b6ff8bf0e4ac6684fe7583c63d7824a9db6e0d
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2025-02-27 23:39:25 +0530

    add path aliases for improved module resolution in TypeScript and Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      "@api": path.resolve(__dirname, './src/api'),
+      "@assets": path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      "@context": path.resolve(__dirname, './src/context'),
+      "@hooks": path.resolve(__dirname, './src/hooks'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      "@sections": path.resolve(__dirname, './src/sections'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@stores': path.resolve(__dirname, './src/stores'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
+});
