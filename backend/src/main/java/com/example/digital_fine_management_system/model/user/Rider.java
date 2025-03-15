1e9759962604d106b86f5cbb5573057b64444563
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2025-03-15 11:15:59 +0530

    fix rider entity

--- backend/src/main/java/com/example/digital_fine_management_system/model/user/Rider.java
+++ backend/src/main/java/com/example/digital_fine_management_system/model/user/Rider.java
@@ -1,9 +1,11 @@
 package com.example.digital_fine_management_system.model.user;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Rider extends User {
     @NotNull(message = "License id cannot be null")
     @Size(min = 1, max = 20, message = "License id must be between 1 and 20 characters")
--- backend/src/main/java/com/example/digital_fine_management_system/repository/user/PoliceOfficerRepository.java
+++ backend/src/main/java/com/example/digital_fine_management_system/repository/user/PoliceOfficerRepository.java
@@ -2,10 +2,11 @@ package com.example.digital_fine_management_system.repository.user;
 
 import com.example.digital_fine_management_system.model.user.PoliceOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PoliceOfficerRepository extends JpaRepository<PoliceOfficer, Long> {
     Optional<PoliceOfficer> findByPoliceId(String policeId);
 }
