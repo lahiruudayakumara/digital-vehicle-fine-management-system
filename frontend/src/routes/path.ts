1615c9f64b74089625507c3f0923eea345aaf77f
Udari Devindi <134499762+UdariDevindi@users.noreply.github.com>
2025-03-17 17:09:44 +0530

    Set up the new paths created

--- frontend/src/routes/path.ts
+++ frontend/src/routes/path.ts
@@ -2,6 +2,7 @@ import LoginPage from "@pages/auth/division-admin/login";
 import DashboardPage from "@pages/dashboard/division-admin/index"; 
 import OfficerManagePage from "@/pages/officer-management/division-admin/officer-management"
 import NewOfficerPage from "@pages/add-new-officer/addOfficerForm"; 
+import UpdateOfficerPage from "@pages/update-officer/update-officer-form"; 
 
 import { createBrowserRouter } from "react-router-dom";
 
@@ -22,5 +23,9 @@ export const router = createBrowserRouter([
     {
         path: "/addnewofficer",
         Component: NewOfficerPage,
+    },
+    {
+        path: "/updateofficer",
+        Component: UpdateOfficerPage,
     },
 ]);
