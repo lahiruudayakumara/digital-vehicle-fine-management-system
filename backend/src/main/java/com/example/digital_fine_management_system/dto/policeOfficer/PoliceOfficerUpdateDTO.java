1a56633a5f6261ab4cd44ea0a62ff89d53dd586e
Udari Devindi <134499762+UdariDevindi@users.noreply.github.com>
2025-03-18 11:54:21 +0530

    Police Officer Update DTO

--- backend/src/main/java/com/example/digital_fine_management_system/dto/policeOfficer/PoliceOfficerUpdateDTO.java
+++ backend/src/main/java/com/example/digital_fine_management_system/dto/policeOfficer/PoliceOfficerUpdateDTO.java
@@ -1,4 +1,37 @@
 package com.example.digital_fine_management_system.dto.policeOfficer;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class PoliceOfficerUpdateDTO {
+
+    @NotBlank(message = "Full name is required")
+    @Size(min = 3, max = 50, message = "Full name must be between 3 and 50 characters")
+    private String fullName;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Telephone number is required")
+    private String telephoneNumber;
+
+    @NotEmpty(message = "Patrol locations are required")
+    private List<String> patrolLocations;
 }
