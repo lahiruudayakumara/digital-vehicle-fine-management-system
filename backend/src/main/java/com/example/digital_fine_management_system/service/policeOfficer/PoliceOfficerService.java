96defa5b05bf4bea9dc722293b74c44bbde3f5bd
Udari Devindi <134499762+UdariDevindi@users.noreply.github.com>
2025-03-18 11:26:32 +0530

    Police officer service IMplementation

--- backend/src/main/java/com/example/digital_fine_management_system/service/policeOfficer/PoliceOfficerService.java
+++ backend/src/main/java/com/example/digital_fine_management_system/service/policeOfficer/PoliceOfficerService.java
@@ -1,4 +1,8 @@
 package com.example.digital_fine_management_system.service.policeOfficer;
 
+import com.example.digital_fine_management_system.dto.policeOfficer.PoliceOfficerResponseDTO;
+import java.util.List;
+
 public interface PoliceOfficerService {
+    List<PoliceOfficerResponseDTO> getAllPoliceOfficers();
 }
--- backend/src/main/java/com/example/digital_fine_management_system/service/policeOfficer/PoliceOfficerServiceImpl.java
+++ backend/src/main/java/com/example/digital_fine_management_system/service/policeOfficer/PoliceOfficerServiceImpl.java
@@ -1,4 +1,36 @@
 package com.example.digital_fine_management_system.service.policeOfficer;
 
-public class PoliceOfficerServiceImpl {
+import com.example.digital_fine_management_system.dto.policeOfficer.PoliceOfficerResponseDTO;
+import com.example.digital_fine_management_system.model.user.PoliceOfficer;
+import com.example.digital_fine_management_system.repository.user.PoliceOfficerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PoliceOfficerServiceImpl implements PoliceOfficerService {
+
+    private final PoliceOfficerRepository policeOfficerRepository;
+
+    @Autowired
+    public PoliceOfficerServiceImpl(PoliceOfficerRepository policeOfficerRepository) {
+        this.policeOfficerRepository = policeOfficerRepository;
+    }
+
+    @Override
+    public List<PoliceOfficerResponseDTO> getAllPoliceOfficers() {
+        List<PoliceOfficer> officers = policeOfficerRepository.findAll();
+
+        return officers.stream().map(officer -> PoliceOfficerResponseDTO.builder()
+                .badgeID(officer.getBadgeID())
+                .fullName(officer.getFullName())
+                .address(officer.getAddress())
+                .telephone(officer.getTelephone())
+                .patrolLocations(Collections.singletonList(officer.getPatrolLocations()))
+                .build()
+        ).collect(Collectors.toList());
+    }
 }
