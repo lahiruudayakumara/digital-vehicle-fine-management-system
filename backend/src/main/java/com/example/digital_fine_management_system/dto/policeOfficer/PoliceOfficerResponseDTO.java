dba956aeab1844b718d750261b5445884a437b7e
Udari Devindi <134499762+UdariDevindi@users.noreply.github.com>
2025-03-18 11:25:29 +0530

    PoliceOfficerResponseDTO for retrieving

--- backend/src/main/java/com/example/digital_fine_management_system/dto/policeOfficer/PoliceOfficerResponseDTO.java
+++ backend/src/main/java/com/example/digital_fine_management_system/dto/policeOfficer/PoliceOfficerResponseDTO.java
@@ -1,33 +1,15 @@
 package com.example.digital_fine_management_system.dto.policeOfficer;
 
-import com.example.digital_fine_management_system.model.user.PoliceOfficer;
-import lombok.Getter;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Data;
 import java.util.List;
 
-@Getter
-@Setter
+@Data
+@Builder
 public class PoliceOfficerResponseDTO {
-    private Long id;
-    private String policeId;
-    private String name;
+    private String badgeID;
+    private String fullName;
     private String address;
-    private String telephoneNumber;
+    private String telephone;
     private List<String> patrolLocations;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
-    // Constructor that takes PoliceOfficer entity
-    public PoliceOfficerResponseDTO(PoliceOfficer officer) {
-        this.id = officer.getId();
-        this.policeId = officer.getPoliceId();
-//        this.name = officer.getName();
-//        this.address = officer.getAddress();
-//        this.telephoneNumber = officer.getTelephoneNumber();
-//        this.patrolLocations = officer.getPatrolLocations();
-        this.createdAt = officer.getCreatedAt();
-        this.updatedAt = officer.getUpdatedAt();
-    }
 }
