2d17129b179ef6f8fc83350c69177917ac1f478d
LAHIRU UDAYAKUMARA <79270918+lahiruudayakumara@users.noreply.github.com>
2025-03-20 17:11:44 +0530

    add scanner screen and button component for QR code scanning functionality

---
+++ mobile-officer/app/scanner/scanner.tsx
@@ -0,0 +1,101 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
+
+import { AppDispatch } from "@/stores/store";
+import { setScannedData } from "@/stores/slices/scannerSlice";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
+
+export default function ScannerScreen() {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+  const [scanned, setScanned] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="Grant Permission" />
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({
+    type,
+    data,
+  }: {
+    type: string;
+    data: string;
+  }) => {
+    setScanned(true);
+    dispatch(setScannedData(data));
+  };
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.cameraContainer}>
+        <CameraView
+          style={styles.camera}
+          facing={facing}
+          barcodeScannerSettings={{
+            barcodeTypes: ["qr"],
+          }}
+          onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+        />
+      </View>
+      {scanned && (
+        <Button title="Tap to Scan Again" onPress={() => setScanned(false)} />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  cameraContainer: {
+    width: 250,
+    height: 250,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  camera: {
+    width: "100%",
+    height: "100%",
+  },
+  message: {
+    textAlign: "center",
+    paddingBottom: 10,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
