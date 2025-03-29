ef1dc00e060b3dd2f0e75121923b505b2382aee1
N A D NIMSARA <duvindunimsara@gmail.com>
2025-03-30 00:24:59 +0530

    Add fines reducer to rootReducer for state management

--- mobile-officer/stores/reducers.ts
+++ mobile-officer/stores/reducers.ts
@@ -1,11 +1,14 @@
+
 import authReducer from "./slices/auth/auth-slice";
 import { combineReducers } from "@reduxjs/toolkit";
 import scannerReducer from "./slices/scannerSlice";
+import finesReducer from './slices/fine/fine-slice'; // Import the fines reducer
 
 const rootReducer = combineReducers({
   auth: authReducer,
   scanner: scannerReducer,
+  fines: finesReducer, // Add the fines reducer here
 });
 
-export default rootReducer;
 export type RootState = ReturnType<typeof rootReducer>;
+export default rootReducer;
